a71a818e58db1a9f00e8616979cdc2d5db63c512
Your Name <you@example.com>
2025-05-20 16:24:26 +0300

    Initial commit on main

---
+++ milestone_1_challenges.js
@@ -0,0 +1,19 @@
+// Challenge 1: Sum of two numbers
+function add(num1, num2){
+    return num1 + num2
+}
+let sum = add(4, 7);node
+console.log('The sum of two numbers : ');
+console.log (sum);
+// end of sum of two numbers.
+
+
+// challenge 2:  Convert Minutes into Seconds
+function convert(minutes){
+    return minutes * 60;
+}
+let minutes = 34;
+let seconds = convert(minutes);
+console.log('The number of seconds in 34 minutes is: ');
+console.log (seconds);
+// end of converting minutes into seconds
