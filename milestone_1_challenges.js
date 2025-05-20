fd16dfdbc70f645a99269467ffe67bc6db200b51
Your Name <you@example.com>
2025-05-20 19:11:32 +0300

    added challenge8 ,9 and 10 using if--else, and if---elseif---else

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -87,7 +87,6 @@ console.log (largest2);
 function calculateBMI(weight, height) {
     let bmi = weight / (height * height);
     let category;
-
     if (bmi < 18.5) {
         category = "Underweight";
     } else if (bmi >= 18.5 && bmi < 24.9) {
@@ -97,15 +96,61 @@ function calculateBMI(weight, height) {
     } else {
         category = "Obese";
     }
-
     return `Your BMI is ${bmi.toFixed(2)} ${category}.`;
 }
-
 console.log(calculateBMI(85, 1.8));  //Your BMI is 26.23 Overweight.
 console.log(calculateBMI(70, 1.75)); //Your BMI is 22.86 Normal weight.
 console.log(calculateBMI(50, 1));    //Your BMI is 50.00 Obese.
 // end of challenge 7
 
 
+// start of challenge 8:  Greet the user based on the time of day
+function greetUser(name, hour) {
+    if (hour >= 5 && hour <= 11) {
+        return `Good morning, ${name}!`;
+    } else if (hour >= 12 && hour <= 17) {
+        return `Good afternoon, ${name}!`;
+    } else if (hour >= 18 && hour <= 21) {
+        return `Good evening, ${name}!`;
+    } else {
+        return `Good night, ${name}!`;
+    }
+}
+console.log(greetUser("maish", 10)); 
+console.log(greetUser("maish", 14)); 
+console.log(greetUser("maish", 19)); 
+console.log(greetUser("maish", 23));
+// end of challenge 8 
 
+// challenge 9:  . FizzBuzz if a number is divisible b 3,5 ,both or not divisible
+function fizzBuzzCheck(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "FizzBuzz";
+    } else if (number % 3 === 0) {
+        return "Fizz";
+    } else if (number % 5 === 0) {
+        return "Buzz";
+    } else {
+        return number.toString();
+    }
+}
+console.log(fizzBuzzCheck(30)); 
+console.log(fizzBuzzCheck(3));  
+console.log(fizzBuzzCheck(5)); 
+console.log(fizzBuzzCheck(1));  
+// end of challenge 9
+
+// challenge 10: Perimeter of a shape;
+// here am to give value to shapes first;
+function calcperimeter(shape, num) {
+    if (shape === "s") {
+        return 4 * num;
+    } else if (shape === "c") {
+        return 6.28 * num;
+    } else {
+        return " Invalid shape";
+    }
+}
+console.log(calcperimeter("s", 8)); //32
+console.log(calcperimeter("c", 3)); //18.84 
 //( node milestone_1_challenges.js    ) //for my purpose of running my code 
