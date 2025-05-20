ab613745eb8c7c477cecaad3ecc4fdb5e423de5c
Your Name <you@example.com>
2025-05-20 17:14:46 +0300

    added challenge 3

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -2,9 +2,12 @@
 function add(num1, num2){
     return num1 + num2
 }
-let sum = add(4, 7);node
-console.log('The sum of two numbers : ');
+let sum = add(4, 7);
+console.log('The sum of two numbers is : ');
 console.log (sum);
+let sum2 = add(10, 20);
+console.log('The sum of two numbers is : ');
+console.log (sum2);
 // end of sum of two numbers.
 
 
@@ -16,4 +19,26 @@ let minutes = 34;
 let seconds = convert(minutes);
 console.log('The number of seconds in 34 minutes is: ');
 console.log (seconds);
+let minutes2 = 2;
+let seconds2 = convert(minutes2);
+console.log('The number of seconds in 2 minutes is: ');
+console.log (seconds2);
 // end of converting minutes into seconds
+
+// challenge 3 : Perimeter of a Rectangle
+function findperimeter(length, width){
+    return 2 * (length + width);
+}
+let length = 6;
+let width = 7;
+let perimeter = findperimeter(length, width);
+console.log('The perimeter of the rectangle is: ');
+console.log (perimeter);
+let length2 = 10;
+let width2 = 20;
+let perimeter2 = findperimeter(length2, width2);
+console.log('The perimeter of the rectangle is: ');
+console.log (perimeter2);
+// end of perimeter of a rectangle challenge
+
+
