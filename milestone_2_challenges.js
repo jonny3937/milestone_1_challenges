0fbb50b1c6f7588ab4e7649a30ae9f51b71506ae
Your Name <you@example.com>
2025-05-21 19:21:31 +0300

    added upto challenge 11 used if statements and for loop

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -1,4 +1,8 @@
 //challenge 1 Sum of Positives
+// Write a function called sumOfPositives that takes an array of numbers and returns the sum of all the positive numbers in the array.
+
+// sumOfPositives([1, -3, 5, -2, 9, -8]
+// ); // 15
 function sumOfPositives(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
@@ -8,9 +12,7 @@ function sumOfPositives(arr) {
     }
     return sum;
 }
-
-    console.log(sumOfPositives([123])); 
-
+console.log(sumOfPositives([40, -43, 25, -14, 2, 4]));
     //end of challenge 1
 
 //challenge 2 find max value
@@ -99,7 +101,68 @@ function sumEvenNumbers(arr) {
     return sum;
 }
 console.log(sumEvenNumbers([10,11,12,13,14,15,16,17]));
-//the gitend of challenge 7
+//the end of challenge 7
+
+//start of challenge 8 Difference Between Sum of Even and Odd Numbers
+function differenceEvenOdd(arr) {
+    let sumEven = 0;
+    let sumOdd = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            sumEven += arr[i];
+        } else {
+            sumOdd += arr[i];
+        }
+    }
+    return sumEven - sumOdd;
+}
+console.log(differenceEvenOdd([1, 2, 3, 4, 5])); 
+//end of challenge 8
+
+
+//start of challenge 9 Count Truthy
+function countTruthy(obj) {
+    let count = 0;
+    for (let key in obj) {
+        if (obj[key]) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countTruthy({ a: 1, b: 0, c: false, d: "hello" }));
+console.log(countTruthy({ a: 1, b: 2, c: 3, d: 4, e: 5 }));
+//end of challenge 9
+
+
+//start of challenge 10  Average of Numbers
+function average(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(average([3, 5, 7, 9]));
+//end of challenge 10
+
+//challenge 11Linear Search
+
+function linearSearch(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+console.log(linearSearch([1, 2, 3, 4, 5], 3)); 
+console.log(linearSearch([1, 2, 3, 4, 5], 6));
+console.log(linearSearch([1, 2, 3, 4, 5], 1));
+//end of challenge 11
 
 
 
